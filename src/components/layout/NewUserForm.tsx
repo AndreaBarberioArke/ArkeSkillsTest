14f7acb8ca196a8eaff6bc3d2294d0d421e7f281
Andrea <andreabarberio@MBP-di-Andrea.station>
2022-11-16 18:03:06 +0100

    Update: fixed uncontrolled form being controlled, now it adds Users to the table with callbacks

--- src/components/layout/NewUserForm.tsx
+++ src/components/layout/NewUserForm.tsx
@@ -1,61 +1,94 @@
-import React from "react";
+import React, { ChangeEventHandler } from "react";
 import Card from "../ui/Card";
 import "./NewUserForm.css";
-import { useRef } from "react";
+import { useState } from "react";
 interface Props {
   onAddUsers: any;
 }
+
 export const NewUserForm = (props: Props) => {
-  const genderInputRef = useRef<any | undefined>();
-  const nameInputRef = useRef<any | undefined>();
-  const locationInputRef = useRef<any | undefined>();
-  const cityInputRef = useRef<any | undefined>();
-  const emailInputRef = useRef<any | undefined>();
+  const [userInfo, setUserInfo] = useState({
+    gender: "",
+    name: "",
+    location: "",
+    city: "",
+    email: "",
+  });
 
   function submitHandler(event: React.FormEvent) {
     event.preventDefault();
-
-    const enteredGender = genderInputRef.current?.value;
-    const enteredName = nameInputRef.current?.value;
-    const enteredLocation = locationInputRef.current?.value;
-    const enteredCity = cityInputRef.current?.value;
-    const enteredEmail = emailInputRef.current?.value;
-
-    const userData = {
-      gender: enteredGender,
-      name: enteredName,
-      location: enteredLocation,
-      city: enteredCity,
-      email: enteredEmail,
-    };
-    props.onAddUsers(userData);
+    props.onAddUsers(userInfo);
   }
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    event.preventDefault();
+    setUserInfo((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
     <Card>
       <form action="" className="form" onSubmit={submitHandler}>
         <div className="control">
-          <label htmlFor="title">New User gender</label>
-          <input type="text" required id="gender" ref={genderInputRef} />
+          <label htmlFor="gender">New User gender</label>
+          <input
+            type="text"
+            required
+            name="gender"
+            id="gender"
+            value={userInfo.gender}
+            onChange={handleChange}
+          />
         </div>
         <div className="control">
-          <label htmlFor="image">New User name</label>
-          <input type="text" required id="name" ref={nameInputRef} />
+          <label htmlFor="name">New User name</label>
+          <input
+            type="text"
+            required
+            name="name"
+            id="name"
+            value={userInfo.name}
+            onChange={handleChange}
+          />
         </div>
         <div className="control">
-          <label htmlFor="address">New User Location</label>
-          <input type="text" required id="location" ref={locationInputRef} />
+          <label htmlFor="location">New User Location</label>
+          <input
+            type="text"
+            required
+            name="location"
+            id="location"
+            value={userInfo.location}
+            onChange={handleChange}
+          />
         </div>
         <div className="control">
           <label htmlFor="city">New User city</label>
-          <input type="text" required id="city" ref={cityInputRef} />
+          <input
+            type="text"
+            required
+            name="city"
+            id="city"
+            value={userInfo.city}
+            onChange={handleChange}
+          />
         </div>
         <div className="control">
           <label htmlFor="email">New User email</label>
-          <input type="text" required id="email" ref={emailInputRef} />
+          <input
+            type="text"
+            required
+            name="email"
+            id="email"
+            value={userInfo.email}
+            onChange={handleChange}
+          />
         </div>
         <div className="control">
           <div className="actions">
-            <button>Add New User</button>
+            <button onClick={submitHandler}>Add New User</button>
           </div>
         </div>
       </form>
--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -8,72 +8,9 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import Paper from "@mui/material/Paper";
-import "../../src/index.css"
+import "../../src/index.css";
 import "../components/layout/NewUserForm.css";
-import { useRef } from "react";
-import Card from "../components/ui/Card"
-interface Props {
-  onAddUsers: any;
-}
-export const NewUserForm = (props: Props) => {
-  const genderInputRef = useRef<any | undefined>();
-  const nameInputRef = useRef<any | undefined>();
-  const locationInputRef = useRef<any | undefined>();
-  const cityInputRef = useRef<any | undefined>();
-  const emailInputRef = useRef<any | undefined>();
-
-  function submitHandler(event: React.FormEvent) {
-    event.preventDefault();
-
-    const enteredGender = genderInputRef.current?.value;
-    const enteredName = nameInputRef.current?.value;
-    const enteredLocation = locationInputRef.current?.value;
-    const enteredCity = cityInputRef.current?.value;
-    const enteredEmail = emailInputRef.current?.value;
-
-    const userData = {
-      gender: enteredGender,
-      name: enteredName,
-      location: enteredLocation,
-      city: enteredCity,
-      email: enteredEmail,
-    };
-    props.onAddUsers(userData);
-  }
-  return (
-    <Card>
-      <form action="" className="form" onSubmit={submitHandler}>
-        <div className="control">
-          <label htmlFor="title">New User gender</label>
-          <input type="text" required id="gender" ref={genderInputRef} />
-        </div>
-        <div className="control">
-          <label htmlFor="image">New User name</label>
-          <input type="text" required id="name" ref={nameInputRef} />
-        </div>
-        <div className="control">
-          <label htmlFor="address">New User Location</label>
-          <input type="text" required id="location" ref={locationInputRef} />
-        </div>
-        <div className="control">
-          <label htmlFor="city">New User city</label>
-          <input type="text" required id="city" ref={cityInputRef} />
-        </div>
-        <div className="control">
-          <label htmlFor="email">New User email</label>
-          <input type="text" required id="email" ref={emailInputRef} />
-        </div>
-        <div className="control">
-          <div className="actions">
-            <button>Add New User</button>
-          </div>
-        </div>
-      </form>
-    </Card>
-  );
-};
-
-
+import NewUserForm from "../components/layout/NewUserForm";
 
 function TablePage() {
   const [user, setUser] = useState<any[]>([]);
@@ -117,10 +54,9 @@ function TablePage() {
         </TableContainer>
       </div>
       <div className="userFormContainer">
-        
         <NewUserForm
-          onAddUsers={() => {
-            console.log("hello");
+          onAddUsers={(userInfo: any) => {
+            setUser((prevState) => [...user, userInfo]);
           }}
         />
       </div>
